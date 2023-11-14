756ab7bdd5bc78e396dcb331ca48f1728083bb90
valiantlynx <86688436+valiantlynx@users.noreply.github.com>
2023-11-14 20:19:58 +0100

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,3 @@
+git subtree add --prefix=apps/pocketbase-docker https://github.com/valiantlynx/pocketbase-docker.git valiantlynx-turborepo --squash
+git subtree pull --prefix=apps/pocketbase-docker https://github.com/valiantlynx/pocketbase-docker.git valiantlynx-turborepo --squash
+git subtree push --prefix=apps/pocketbase-docker https://github.com/valiantlynx/pocketbase-docker.git valiantlynx-turborepo
